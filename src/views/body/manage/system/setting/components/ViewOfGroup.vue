c198f6cc7f3152937e2afe91a60e97f1c4e426a2
方某方 <login@kantboot.com>
2023-11-21 16:48:06 +0800

    小改

---
+++ src/views/body/manage/system/setting/components/ViewOfGroup.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+</script>
+
+<template>
+<el-card>
+  <template #header>
+    分组
+  </template>
+  <div>
+    <el-button style="width: 100%;">
+      <kt-icon icon="el-icon-plus" />
+    </el-button>
+  </div>
+</el-card>
+</template>
+
+<style scoped>
+</style>
